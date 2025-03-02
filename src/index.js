062d8e151f64e4404cd6e9d1eb220222c994de87
leunghoiyi <leunghoiyii@gmail.com>
2025-03-02 19:15:49 +0200

    Add API Integration

--- src/index.js
+++ src/index.js
@@ -11,45 +11,59 @@ function search(event) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
-}
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector(".current-temperature-value");
   let currentTemperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = currentTemperature;
+  let conditionElement = document.querySelector(".condition");
+  conditionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector(".wind");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let iconElement = document.querySelector(".current-temperature-icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 
-  console.log(response.data);
+  console.log(response);
+  console.log(response.data.time);
+
+  function formatDate(currentDate) {
+    let minutes = currentDate.getMinutes();
+    let hours = currentDate.getHours();
+    let day = currentDate.getDay();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
+
+  let currentDateElement = document.querySelector(".current-date");
+  let currentDate = new Date(response.data.time * 1000);
+  currentDateElement.innerHTML = formatDate(currentDate);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
+window.onload = function () {
+  let searchInputElement = document.querySelector("#search-input");
+  searchInputElement.value = "Tallinn"; // Set the input value to Tallinn
+  search(new Event("submit")); // Trigger the search function as if the form is submitted
+};
